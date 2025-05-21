6b96912da08c4c4838b87775bd1dbfba42f5aef0
nandowastaken <fernando.jorge@orla.tech>
2025-05-21 14:20:36 -0300

    feat: two separated commands

--- main.go
+++ main.go
@@ -8,12 +8,32 @@ import (
 
 func main() {
 	green := "\033[32m"
+
+	changeOrigin := flag.String("c", "false", "commits to two repositories")
 	commitMessage := flag.String("m", "World", "name to greet")
 	flag.Parse()
 
-	exec.Command("git", "add", "*").Run()
-	exec.Command("git", "commit", "-m", *commitMessage).Run()
-	exec.Command("git", "push").Run()
+	if *changeOrigin == "false" {
+		commitPush(*commitMessage)
+	} else {
+
+	}
 
 	fmt.Println(green, "Pushed!")
+}
+
+func commitPushTwoRepos(commitMessage string) {
+	commitPush(commitMessage)
+	exec.Command("git", "remote", "rm", "origin").Run()
+	exec.Command("git", "remote", "add", "origin").Run()
+
+	commitPush(commitMessage)
+	exec.Command("git", "remote", "rm", "origin").Run()
+	exec.Command("git", "remote", "add", "origin").Run()
+}
+
+func commitPush(commitMessage string) {
+	exec.Command("git", "add", "*").Run()
+	exec.Command("git", "commit", "-m", commitMessage).Run()
+	exec.Command("git", "push").Run()
 }
